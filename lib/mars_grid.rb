5f0488744d7ef4bdf60815ae8fc4936ec3c9288a
Fareed Patel <fareed_patel@hotmail.co.uk>
2016-09-18 23:06:00 +0100

    limits the rover to the grid boundary- tests passing

--- lib/mars_grid.rb
+++ lib/mars_grid.rb
@@ -6,6 +6,13 @@ class MarsGrid
     grid_size
   end
 
+  def within_bounds?(x, y)
+    x >= 0 and x <= @height and
+    y >= 0 and y <= @width
+  end
+
+
+private
   def grid_size
     @height = @coordinates.shift.to_i
     @width = @coordinates.shift.to_i
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,4 +1,5 @@
 require 'rover'
+require 'mars_grid'
 describe Rover do
   let (:input) { ['1', '2', 'N', 'LMLMLMLMM'] }
 
@@ -8,20 +9,51 @@ describe Rover do
   end
 
   it 'turns right when given R instruction' do
-    @rover.do 'R'
+    @rover.take_action 'R'
     expect(@rover.current_position[:face]).to eq('E')
   end
 
   it 'turn left when given L instruction' do
-    @rover.do 'L'
+    @rover.take_action 'L'
     expect(@rover.current_position[:face]).to eq('W')
   end
 
   it 'moves forward one when given instruction M' do
-    @rover.do 'M'
+    @rover.take_action 'M'
     expect(@rover.current_position[:x]).to eq(1)
     expect(@rover.current_position[:y]).to eq(3)
+
+    @rover.take_action 'R'
+    @rover.take_action 'M'
+    expect(@rover.current_position[:x]).to eq(2)
+    expect(@rover.current_position[:y]).to eq(3)
+
+    @rover.take_action 'L'
+    @rover.take_action 'M'
+    expect(@rover.current_position[:x]).to eq(2)
+    expect(@rover.current_position[:y]).to eq(4)
   end
 
+  it 'moves around' do
+    @rover.move
+    expect(@rover.current_position).to eq :x=>1, :y=>3, :face=>"N"
+  end
 
+  it 'converts it to a string' do
+    @rover.move
+    expect(@rover.to_string).to eq ('1 3 N')
+  end
+
+  it 'does not allow the rover get out of the mars grid' do
+    5.times { @rover.take_action 'M' }
+    @rover.take_action 'R'
+    5.times { @rover.take_action 'M' }
+    expect(@rover.current_position).to eq(x: 5, y: 5, face: 'E')
+
+    @rover.take_action 'L'
+    5.times { @rover.take_action 'M' }
+    @rover.take_action 'L'
+    5.times { @rover.take_action 'M' }
+    expect(@rover.current_position).to eq(x: 0, y: 5, face: 'W')
+  end
 end
